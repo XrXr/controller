ee286b741281dbf5a937a750483bfc3399f4725d
Alan Wu <XrXr@users.noreply.github.com>
2020-12-23 03:56:07 -0500

    Update config
    Layouts are checked into this repo instead of kll because kll is now a
    Python package and layouts can't be added to it easily.
    
    Layouts now work with new version of kll and capslock can now give page
    up, page down, home, end, control and shift in addtion to arrow keys.
    
    Again, run Keyboards/k-type.bash to build.

--- Keyboards/k-type.bash
+++ Keyboards/k-type.bash
@@ -27,7 +27,7 @@ BaseMap="scancode_map"
 # This is the default layer of the keyboard
 # NOTE: To combine kll files into a single layout, separate them by spaces
 # e.g.  DefaultMap="mylayout mylayoutmod"
-DefaultMap="k-type/release.1 alan/k-type-base stdFuncMap"
+DefaultMap="alan/k-type-base stdFuncMap"
 
 # This is where you set the additional layers
 # NOTE: Indexing starts at 1
@@ -36,7 +36,7 @@ DefaultMap="k-type/release.1 alan/k-type-base stdFuncMap"
 #       PartialMaps[2]="layer2"
 #       PartialMaps[3]="layer3"
 PartialMaps[1]="alan/layer1"
-PartialMaps[2]="alan/home-row-vim"
+PartialMaps[2]="alan/caps-nav"
 
 
 
